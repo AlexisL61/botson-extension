7c68067cc5a4c4e28aed73aa5496bfdae2329b64
AlexisL61 <alexisleray61@outlook.com>
2020-08-04 21:09:21 +0200

    Ajout de l'extension de bienvenue

---
+++ welcome-goodbye-extension/back-end/main.js
@@ -0,0 +1,30 @@
+var client,dataFolder,electron
+const fs = require("fs")
+module.exports = {
+    start(){
+        client = this.client
+        dataFolder = this.dataFolder
+        electron = this.electron
+        var configData = JSON.parse(fs.readFileSync(dataFolder+"/webpage-data/config.json","utf-8"))
+        client.on("guildMemberAdd",function(member){
+            if (configData[member.guild.id] && configData[member.guild.id].welcome.channel != "default"){
+                if (member.guild.channels.cache.has(configData[member.guild.id].welcome.channel)){
+                    var thisChannel = member.guild.channels.cache.get(configData[member.guild.id].welcome.channel)
+                    var messageToSend = configData[member.guild.id].welcome.message
+                    messageToSend=messageToSend.replace("{member}","<@"+member.id+">")
+                    thisChannel.send(messageToSend)
+                }
+            }
+        })
+        client.on("guildMemberRemove",function(member){
+            if (configData[member.guild.id] && configData[member.guild.id].goodbye.channel != "default"){
+                if (member.guild.channels.cache.has(configData[member.guild.id].goodbye.channel)){
+                    var thisChannel = member.guild.channels.cache.get(configData[member.guild.id].goodbye.channel)
+                    var messageToSend = configData[member.guild.id].goodbye.message
+                    messageToSend=messageToSend.replace("{member}","<@"+member.id+">")
+                    thisChannel.send(messageToSend)
+                }
+            }
+        })
+    }
+}
